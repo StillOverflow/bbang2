ba5137d91213cdbec6c7858a02d0d4e4e952d52d
kanghyeon2 <topskh@naver.com>
2024-12-12 15:30:08 +0900

    수정:폴더정리

---
+++ vue/src/views/Bom.vue
@@ -0,0 +1,38 @@
+<script setup>
+import BomProduct from "./components/BomProduct.vue";
+import BomMaterial from "./components/BomMaterial.vue";
+import BomInformation from "./components/BomInformation.vue";
+</script>
+
+<!-- BOM -->
+
+<!-- 설비 -->
+<template>
+  <div>
+    <!-- 템플릿 내용 -->
+    <div class="py-4 container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <BomProduct />
+        </div>
+      </div>
+      <div class="mt-4 row">
+        <div class="col-12">
+          <BomMaterial />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <BomInformation />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Bom",
+  // 컴포넌트 로직
+};
+</script>
