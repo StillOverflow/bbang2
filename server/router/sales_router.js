ea58a116d042e96431af65dcc37332002f054166
Soonbum <shsrhdtnsqja@naver.com>
2024-12-16 23:11:53 +0900

    수정:주문서등록페이지

--- server/router/sales_router.js
+++ server/router/sales_router.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const salesService = require("../service/sales_service.js");
 
-//주문서 목록
+/* ---------------------------주문서--------------------------------- */
+//주문서 목록 조회
 router.get('/sales', async (req, res) => {
     try {
         let orderList = await salesService.listOrder();
@@ -13,7 +14,7 @@ router.get('/sales', async (req, res) => {
     
 });
 
-//주문서목록거래처검색
+//주문서 목록 검색
 router.get('/sales/search/', async (req, res) => {
     try {
         let salesNo = req.query.no;
@@ -28,5 +29,12 @@ router.get('/sales/search/', async (req, res) => {
     
 });
 
+//주문서 등록
+
+//거래처 조회(모달)
+router.get('/moacc', async (req, res)=>{
+    let moAccList = await salesService.listAccMo();
+    res.send(moAccList);
+});
 
 module.exports = router;
--- vue/src/views/sales/Sales_OrderAdd.vue
+++ vue/src/views/sales/Sales_OrderAdd.vue
@@ -1,22 +1,70 @@
 <!-- 주문서 등록 -->
+<style>
+.form-control[readonly] {
+    background-color: rgb(236, 236, 236);
+}
+</style>
 <template>
     <div class="py-4 container-fluid">
         <div class="card">
             <!-- 주문 테이블 부분 -->
             <div class="card-header bg-light ps-5 ps-md-4">
+                <div class="row">
+                    <div class="col-6 col-lg-2 text-end text-md-start">
+                        <button class="btn btn-primary " @click="modalOpen">거래처 조회</button>
+                    </div>
+                    <div class="col-6 col-lg-1 text-center mb-2 mt-2 fw-bolder" :style="t_overflow">거래처 명</div> 
+                    <div class="col-6 col-lg-2">
+                        <input class="form-control" type="text" value="" readonly />
+                    </div>
+                    <div class="col-6 col-lg-1 text-center mb-2 mt-2 fw-bolder" :style="t_overflow">거래처 코드</div> 
+                    <div class="col-6 col-lg-2">
+                        <input class="form-control" type="text" value="" readonly />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 col-lg-2 text-end text-md-start">
+                        <button class="btn btn-primary " @click="modalOpen">담당자 조회</button>
+                    </div>
+                    <div class="col-6 col-lg-1 text-center mb-2 mt-2 fw-bolder" :style="t_overflow">담당자 명</div> 
+                    <div class="col-6 col-lg-2">
+                        <input class="form-control" type="text" value="" readonly />
+                    </div>
+                    <div class="col-6 col-lg-1 text-center mb-2 mt-2 fw-bolder" :style="t_overflow">담당자 ID</div> 
+                    <div class="col-6 col-lg-2">
+                        <input class="form-control" type="text" value="" readonly />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 col-lg-2"></div>
+                    <div class="col-6 col-lg-1 text-center mb-2 mt-2 fw-bolder" :style="t_overflow">납기일자</div> 
+                    <div class="col-6 col-lg-2">
+                        <input class="form-control" type="date" value="" />
+                    </div>
+                    <div class="col-6 col-lg-1 text-center mb-2 mt-2 fw-bolder" :style="t_overflow">주문일자</div> 
+                    <div class="col-6 col-lg-2">
+                        <input class="form-control" type="date" value="" />
+                    </div>
+                </div>
             </div>
             <!-- 주문디테일 테이블 부분 -->
             <div class="card-body">
-              <ag-grid-vue style="width:100%; height: 300px;"
-              class="ag-theme-alpine"
-              :columnDefs="columnDefs"
-              :rowData="rowData"
-              :gridOptions="gridOptions"
-              @grid-ready="gridFit">
-              </ag-grid-vue>
+                <div class="row">
+                    <div class="col-6 col-lg-2 text-end text-md-start">
+                        <button class="btn btn-primary " @click="modalOpen">제품 조회</button>
+                    </div>
+                </div>
+                <ag-grid-vue style="width:100%; height: 440px;"
+                class="ag-theme-alpine"
+                :columnDefs="columnDefs"
+                :rowData="rowData"
+                :gridOptions="gridOptions"
+                @grid-ready="gridFit">
+                </ag-grid-vue>
             </div>
         </div>
     </div>
+
 </template>
   
 <script>
@@ -33,6 +81,9 @@ export default {
     },
     components: {
         AgGridVue
+    },
+    created() {
+        this.$store.dispatch('breadCrumb', { title: '주문서 등록' });
     },
     beforeMount() {
         this.columnDefs = [
@@ -58,10 +109,10 @@ export default {
 }
 </script>
   
-  <style lang="scss">
-  //그리드 사용시 아래 스타일 임포트 해야하고 경로 확인이 필요하다 
-  @import"../../../node_modules/ag-grid-community/styles/ag-grid.css";
-  @import"../../../node_modules/ag-grid-community/styles/ag-theme-alpine.css"
-  
-  
-  </style>
+<style lang="scss">
+//그리드 사용시 아래 스타일 임포트 해야하고 경로 확인이 필요하다 
+@import"../../../node_modules/ag-grid-community/styles/ag-grid.css";
+@import"../../../node_modules/ag-grid-community/styles/ag-theme-alpine.css"
+
+
+</style>
