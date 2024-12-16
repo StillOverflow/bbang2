92b7db9339b7b5a5e9842f1af56f2c14c68698b0
juhyun <gn06146@naver.com>
2024-12-16 10:38:19 +0900

    수정: DB연결

--- server/database/sql.js
+++ server/database/sql.js
@@ -1,17 +1,7 @@
 const common = require("./sqls/common.js");
-const equipment = require("./sqls/equipment.js");
-const standard = require("./sqls/standard.js");
-const material = require("./sqls/material.js");
 const produce = require("./sqls/produce.js");
-const quality = require("./sqls/quality.js");
-const sales = require("./sqls/sales.js");
 
 module.exports = {
   ...common,
-  ...equipment,
-  ...standard,
-  ...material,
   ...produce,
-  ...quality,
-  ...sales,
 };
