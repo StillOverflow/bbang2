ce4cf8fd7bf669e84b4b603a70ab4ace1d757a94
seojin <admin@DESKTOP-U820E4V>
2024-12-16 10:53:35 +0900

    수정: view에 /api/ 추가

--- server/database/sqls/equipment.js
+++ server/database/sqls/equipment.js
@@ -1,28 +1,23 @@
 /* -----------설비------------*/
 
 //상태전체조회
-const eqStatList =
-`SELECT * FROM equipment`;
+const eqStatList = `SELECT * FROM equipment`;
 
 //정보전체조회
-const eqAllList =
-`SELECT * FROM equipment`;
+const eqAllList = `SELECT * FROM equipment`;
 
 //등록
-const eqInsert =
-`INSERT INTO equipment 
+const eqInsert = `INSERT INTO equipment 
 SET ? `;
 
 //수정
-const eqUpdate =
-`UPDATE equipment 
+const eqUpdate = `UPDATE equipment 
 SET ? 
 WHERE EQP_CD = ?`;
-
 
 module.exports = {
   eqStatList,
   eqAllList,
   eqInsert,
-  eqUpdate
+  eqUpdate,
 };
