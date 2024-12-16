0272c2af7f647802d0d8dc49429b769977ae48c0
juhyun <gn06146@naver.com>
2024-12-16 10:53:48 +0900

    수정: service 변수명 수정

--- server/service/produce_service.js
+++ server/service/produce_service.js
@@ -1,43 +1,43 @@
-const mysql = require("../database/mapper.js");
+const mariadb = require("../database/mapper.js");
 
 /*--------------계획서-------------*/
 
 // 전체조회
 const findAllPlan = async ()=>{
-  let list = await mysql.query('planList');
+  let list = await mariadb.query('planList');
   return list;
 }
 
 // 단건조회
 const findPlanNo = async (no)=>{
-  let list = await mysql.query('planInfo', no);
+  let list = await mariadb.query('planInfo', no);
   let info = list[0];
   return info;
 }
 
 // 제품조회
 const findAllPlanDtl = async (no)=>{
-  let list = await mysql.query('planDtlList', no);
+  let list = await mariadb.query('planDtlList', no);
   return list;
 }
 
 /*--------------지시서-------------*/
 // 조회
 const findAllInst = async ()=>{
-  let list = await mysql.query('instList');
+  let list = await mariadb.query('instList');
   return list;
 }
 
 // 단건조회
 const findInstNo = async (no)=>{
-  let list = await mysql.query('instInfo', no);
+  let list = await mariadb.query('instInfo', no);
   let info = list[0];
   return info;
 }
 
 // 등록
 const instInsert = async (instInfo)=>{
-  let result = await mysql.query('instInsert', instInfo);
+  let result = await mariadb.query('instInsert', instInfo);
   if( result.insertId > 0){
     return { inst_cd : result.insertId }; 
   }else{
@@ -47,7 +47,7 @@ const instInsert = async (instInfo)=>{
 
 //제품별 공정 조회
 const findInstFlow = async (no)=>{
-  let list = await mysql.query('instProcList', no);
+  let list = await mariadb.query('instProcList', no);
   return list;
 }
 
