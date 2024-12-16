358d438f5138ec8a6f7123a6172cea379966f3eb
juhyun <gn06146@naver.com>
2024-12-16 16:02:11 +0900

    생성: 생산지시서 등록 검색모달 생성

---
+++ vue/src/views/components/modalLayout.vue
@@ -0,0 +1,93 @@
+<template>
+    <div tabindex="-1" class="modal-wrap" v-show="modalCheck">
+        <div class="modal-container">
+            <header class="modal-header">
+                <slot name="header">
+                    <h5 class="modal-title">BootstrapVue</h5>
+                    <button type="button" aria-label="Close" class="close" @click="modalOpen">×</button>
+                </slot>                
+            </header>
+            <div class="modal-body">
+                <slot><p class="my-4">Hello from modal!</p></slot>                 
+            </div>
+            <footer class="modal-footer">
+                <slot name="footer">
+                    <button type="button" class="btn btn-secondary" @click="modalOpen">Cancel</button>
+                    <button type="button" class="btn btn-primary" @click="modalOpen">OK</button>
+                </slot>                  
+            </footer>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      // modalCheck: false,
+    };
+  },
+  props: {
+    modalCheck : Boolean
+  },
+  methods: {
+    // modalOpen() {
+    //   this.modalCheck = !this.modalCheck
+    // }
+  }
+};
+</script>
+
+<style>
+.modal-wrap {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+  background: rgba(0, 0, 0, 0.4);
+}
+
+/* modal or popup */
+.modal-container {
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 700px;
+  background: #fff;
+  border-radius: 10px;
+  box-sizing: border-box;
+}
+
+.close {
+    float: right;
+    font-size: 1.5rem;
+    font-weight: 700;
+    line-height: 1;
+    color: #000;
+    text-shadow: 0 1px 0 #fff;
+    opacity: .5
+}
+
+.close:hover {
+    color: #000;
+    text-decoration: none
+}
+
+.close:not(:disabled):not(.disabled):focus,.close:not(:disabled):not(.disabled):hover {
+    opacity: .75
+}
+
+button.close {
+    padding: 0;
+    background-color: transparent;
+    border: 0
+}
+
+a.close.disabled {
+    pointer-events: none
+}
+</style>
