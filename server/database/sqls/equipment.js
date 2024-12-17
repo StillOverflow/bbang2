99e980f6d6e033a2e96ca1c1bededc890e56a234
seojin <admin@DESKTOP-U820E4V>
2024-12-17 17:15:46 +0900

    수정: 설비구분,점검구분,설비상태 select 연결 및 모달 연결(ing)

--- server/database/sqls/equipment.js
+++ server/database/sqls/equipment.js
@@ -6,6 +6,29 @@ const eqStatList = `SELECT * FROM equipment`;
 //정보전체조회
 const eqAllList = `SELECT * FROM equipment`;
 
+//단건조회
+const equipInfo = `SELECT (SELECT comm_dtl_nm FROM common_detail WHERE comm_dtl_cd = eqp_type) eqp_type, 
+                          eqp_nm,
+                          model,
+                          pur_dt,
+                          pur_act,
+                          mfg_act,
+                          insp_cycle,
+                          repl_cycle,
+                          id,
+                          opt_temp,
+                          opt_humid,
+                          opt_rpm,
+                          opt_speed,
+                          opt_power,
+                          uph,
+                          (SELECT comm_dtl_nm FROM common_detail WHERE comm_dtl_cd = status) STATUS,
+                          (SELECT comm_dtl_nm FROM common_detail WHERE comm_dtl_cd = is_use) is_use
+
+FROM equipment
+WHERE eqp_cd = ?
+`;
+
 //등록
 const eqInsert = `INSERT INTO equipment 
 SET ? `;
@@ -18,6 +41,7 @@ WHERE EQP_CD = ?`;
 module.exports = {
   eqStatList,
   eqAllList,
+  equipInfo,
   eqInsert,
-  eqUpdate
+  eqUpdate,
 };
