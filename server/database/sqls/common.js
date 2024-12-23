6bc8c8d74f7aca3c61265ec9483b5244a00f9c31
jin ◡̈ <alswls7887@gmail.com>
2024-12-23 17:04:35 +0900

    등록:공통함수(거래처, 자재, 상품)

--- server/database/sqls/common.js
+++ server/database/sqls/common.js
@@ -1,11 +1,4 @@
-// 정확히 특정 공통코드를 찾을 때
-const findComm = `
-  SELECT comm_dtl_nm
-  FROM   common_detail
-  WHERE  UPPER(comm_dtl_cd) = UPPER(?)
-`;
-
-// QQA01 중 'QQ'와 관련된 공통코드를 모두 찾을 때
+//! QQA01 중 'QQ'와 관련된 공통코드를 모두 찾을 때
 const findCommList = `
   SELECT comm_dtl_nm, comm_dtl_cd
   FROM   common_detail
@@ -13,15 +6,82 @@ const findCommList = `
   ORDER  BY comm_dtl_cd
 `;
 
-// select박스에 공통코드 list 뿌릴 때
-const commList = `
-SELECT comm_dtl_nm, comm_dtl_cd
-FROM   common_detail
-WHERE  UPPER(comm_cd) = UPPER( ? )
-`;
+//! 거래처 조회
+// 거래처 코드가 존재하면 Where 조립 없으면 전체조회
+const accountSelect = (datas) => {
+  let query = `
+    SELECT  act_cd, fn_get_codename(act_type) AS act_type, act_nm, mgr_nm, mgr_tel, ceo_nm, 
+            act_addr, act_tel, business_no, location, note
+    FROM    account
+  `;
+
+  const conditions = [];
+
+  // 거래처조회 조건
+  if (datas.act_type) conditions.push(`act_type = UPPER('${datas.act_type}')`);
+  if (datas.act_cd) conditions.push(`act_cd = UPPER('${datas.act_cd}')`);
+  if (datas.act_nm) conditions.push(`act_nm LIKE '%${datas.act_nm}%'`);
+
+  if (conditions.length > 0) {
+    query += ` WHERE ` + conditions.join(' AND ');
+  }
+  
+  query += ` ORDER BY act_cd`; // 정렬
+
+  return query; // 합체한 쿼리 전체
+};
+
+//! 자재 조회
+const materialSelect = (datas) => {
+  let query = `
+    SELECT mat_cd, mat_nm, \`type\`, price, safe_stk, create_dt, update_dt, category, \`unit\`
+    FROM   material
+  `;
+  
+  const conditions = [];
+
+  // 자재조회 조건
+  if(datas.mat_cd) conditions.push(`mat_cd = UPPER('${datas.mat_cd}')`);
+  if(datas.mat_nm) conditions.push(`mat_nm LIKE '%${datas.mat_nm}%'`);
+  if(datas.type) conditions.push(`\`type\` = UPPER('${datas.type}')`);
+  if(datas.category) conditions.push(`\`category\` = UPPER('${datas.category}')`);
+
+  if (conditions.length > 0) {
+    query += ` WHERE ` + conditions.join(' AND ');
+  }
+
+  query += ` ORDER BY mat_cd`;
+
+  return query;
+}
+
+//! 상품 조회
+const productSelect = (datas) => {
+  let query = `
+    SELECT prd_cd, prd_nm, category, price, \`unit\`, exp_range, safe_stk, create_dt, update_dt, note
+    FROM   product
+  `;
+  
+  const conditions = [];
+
+  // 상품조회 조건
+  if(datas.prd_cd) conditions.push(`prd_cd = UPPER('${datas.prd_cd}')`);
+  if(datas.prd_nm) conditions.push(`prd_nm LIKE '%${datas.prd_nm}%'`);
+  if(datas.category) conditions.push(`\`category\` = UPPER('${datas.category}')`);
+
+  if (conditions.length > 0) {
+    query += ` WHERE ` + conditions.join(' AND ');
+  }
+
+  query += ` ORDER BY prd_cd`;
+
+  return query;
+}
 
 module.exports = {
-  findComm,
+  // findComm,
   findCommList,
-  commList
+  accountSelect,  // 거래처
+  materialSelect, // 자재
+  productSelect,  // 제품
 };
