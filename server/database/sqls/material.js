d5759191b7749bdee53b1b2a3521dd5d52a49a76
mjin0302 <alswls7887@gmail.com>
2024-12-30 04:17:47 +0900

    수정:발주서 조회 할 때 쿼리 조건 수정

--- server/database/sqls/material.js
+++ server/database/sqls/material.js
@@ -126,6 +126,7 @@ const getMaterialOrder = `
             fn_get_membername(id) AS id,
             (SELECT act_nm FROM account a WHERE m.act_cd = a.act_cd) AS act_cd
    FROM     material_order m
+   WHERE    UPPER(status) <> UPPER('L01')
    ORDER BY mat_order_cd
 `
 
