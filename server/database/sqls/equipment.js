0a617f910d984d9673eeb917a722e18d71f2717c
seozzini <seozzini@gmail.com>
2024-12-31 06:18:19 +0900

    수정: 설비정보조회 셀렉트>라디오 변경 / 설비점검관리 페이지 수정 작업중

--- server/database/sqls/equipment.js
+++ server/database/sqls/equipment.js
@@ -163,24 +163,24 @@ ORDER BY i.create_dt DESC
 
 //설비점검조회(설비별 최신1건씩만)
 const eqInspListOne = `SELECT 
-  i.insp_log_cd,
-  e.eqp_cd,
-  fn_get_codename(e.eqp_type) AS eqp_type,
-  e.eqp_nm,
-  e.model,
-  e.insp_cycle,
-  e.img_path,
-  e.last_insp_dt,
-  i.start_time,
-  fn_get_codename(i.insp_type) AS insp_type,
-  fn_get_codename(i.insp_reason) AS insp_reason,
-  fn_get_codename(i.insp_result) AS insp_result,
-  i.insp_action,
-  i.note,
-  i.end_time,
-  i.id,
-  i.create_dt,
-  i.update_dt
+  i.insp_log_cd as insp_log_cd,
+  e.eqp_cd as eqp_cd,
+  e.eqp_type as eqp_type,
+  e.eqp_nm as eqp_nm,
+  e.model as model,
+  e.insp_cycle as insp_cycle,
+  e.img_path as img_path,
+  e.last_insp_dt as last_insp_dt,
+  i.start_time as start_time,
+  fn_get_codename(i.insp_type) as insp_type,
+  fn_get_codename(i.insp_reason) as insp_reason,
+  fn_get_codename(i.insp_result) as insp_result,
+  fn_get_codename(i.insp_action) as insp_action,
+  i.note as note,
+  i.end_time as end_time,
+  i.id as id,
+  i.create_dt as create_dt,
+  i.update_dt as update_dt
 FROM equipment e
 LEFT JOIN inspection_log i 
   ON e.eqp_cd = i.eqp_cd
@@ -225,6 +225,55 @@ ORDER BY e.eqp_cd, i.start_time DESC
 limit 1
   `;
 
+  //설비 점검 조회(필터링 적용)
+const eqInstListSearch = (searchObj) => {
+  let query = ` SELECT  e.eqp_cd as eqp_cd,
+                      fn_get_codename(e.eqp_type) as eqp_type,
+                      e.eqp_nm as eqp_nm,
+                      e.insp_cycle as insp_cycle,
+                      e.img_path as img_path,
+                      fn_get_codename(e.is_use) as is_use,
+                      e.model as model,
+                      fn_get_codename(e.status) as status,
+                      e.last_insp_dt as last_insp_dt, 
+                      i.start_time as start_time,
+                      i.insp_type as insp_type,
+                      i.insp_reason as insp_reason,
+                      i.insp_result as insp_result,
+                      i.insp_action as insp_action,
+                      i.note as note,
+                      i.end_time as end_time,
+                      e.id as id,
+                      e.create_dt as create_dt,
+                      e.update_dt as update_dt
+
+FROM equipment e
+      LEFT JOIN inspection_log i 
+              ON e.eqp_cd = i.eqp_cd
+`;
+
+  const conditions = [];
+
+  //설비구분
+  if (searchObj.eqp_type)
+    conditions.push(`eqp_type = '${searchObj.eqp_type}'`);
+
+  //사용유무(사용가능/사용불가)
+  if (searchObj.is_use)
+    conditions.push(`is_use = '${searchObj.is_use}'`);
+
+  //설비상태(가동/비가동)
+  if (searchObj.status)
+    conditions.push(`status = '${searchObj.status}'`);
+
+  // WHERE 절 조립
+  if (conditions.length > 0) {
+    query += ` WHERE ` + conditions.join(' AND ');
+  }
+  // 쿼리 반환
+  return query;
+};
+
 module.exports = {
   eqStatList,
   eqAllList,
@@ -238,5 +287,6 @@ module.exports = {
   getInspCd,
   eqInspInsert,
   eqInspUpdate,
-  eqInspListOne
+  eqInspListOne,
+  eqInstListSearch
 };
